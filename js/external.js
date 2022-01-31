b2c2bf72294d11a8fb00adc4e155145e75c83a05
John Castro <johhncaastro@gmail.com>
2022-01-31 17:02:35 -0600

    complete class exercise external.js and inline.js

---
+++ js/external.js
@@ -0,0 +1,51 @@
+"use strict";
+
+// console.log('Hello from external JavaScript!');
+//
+// alert ('welcome to my website!')
+//
+// var colorChoice = prompt('What is your favorite color?');
+//
+// alert ('Great! '+ colorChoice + ' is my favorite color also!!' )
+
+//the above will tell how to add an alert that will respond with the message that is given
+
+// var littleMermaid = Number(prompt("How many days did you rent Little Mermaid for?"));
+//
+// var brotherBear = Number(prompt("How many days did you rent Brother Bear for?"));
+//
+// var Hercules = Number(prompt("How many days did you rent Hercules for?"));
+//
+// var total = littleMermaid + brotherBear + Hercules;
+//
+// alert("Your total cost for the rentals is " + (total * 3));
+
+// the above lines show how to create prompts for how many rental days and how much its gonna cost
+
+// var googleWorkHours = Number(prompt("How many Hours did you work for Google?"));
+//
+// var googlePay = 400;
+//
+// var amazonWorkHours = Number(prompt('How many hours did you work for Amazon?'));
+//
+// var amazonPay = 380;
+//
+// var facebookWorkHours = Number(prompt('how many hours did you work for Facebook?'));
+//
+// var facebookPay = 350;
+//
+// var totalPay = (googleWorkHours * googlePay) + (amazonWorkHours * amazonPay) + (facebookWorkHours * facebookPay);
+//
+// alert ('you will be paid ' + totalPay + 'USD')
+
+// the above code is to calculate total pay per hour
+
+// var classNotFull = false;
+// var noScheduleConflict = false;
+// var canEnroll = (classNotFull && noScheduleConflict);
+// alert('it is  ' + canEnroll + 'that you can enroll in this class');
+
+
+
+
+
