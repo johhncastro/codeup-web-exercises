8a6a7819c02c0a3970b8f05fbdc084feb1f91a1b
John Castro <johhncaastro@gmail.com>
2022-02-07 12:07:52 -0600

    add js warmup html and js file

---
+++ js/js-warmups.js
@@ -0,0 +1,18 @@
+'use strict';
+// write a function that takes an input and returns the string I have no idea what you just said.
+
+function unknownWords(input){
+    return 'I have no idea what you just said.'
+}
+
+// console.log(unknownWords('loll'));
+// console.log(unknownWords(4));
+
+//write a function that takes in a number and returns a number that is double the input
+
+function numberTimesTwo(num){
+    return num * 2
+}
+
+console.log(numberTimesTwo(4));
+console.log(numberTimesTwo(8));
