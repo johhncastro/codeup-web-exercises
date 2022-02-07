a8c97eaa1649eef88b2d26dd42552865275d9ef8
John Castro <johhncaastro@gmail.com>
2022-02-07 15:50:45 -0600

    add extra notes about do while loop inside loops-lecture.js

--- js/loops-lecture.js
+++ js/loops-lecture.js
@@ -38,12 +38,24 @@
 
 // the above code shows a loop that can multiply a number by another number
 
-var number = 5;
-while (number < 200){
-    console.log(number);
-    number = number * 2;
-}
-console.log('the loop de loop is over')
+// var number = 5;
+// while (number < 200){
+//     console.log(number);
+//     number = number * 2;
+// }
+// console.log('the loop de loop is over')
 
 // this loop only multiplies a number until it hits 200 the reason that it does not go over 200 is because if it goes one more time after 160 it will be over 200 so it will not show up  in the return. the reason for this is because the console log is before the number = number *2; part of the script if it is after it will show 3 hundred something this is because it is running after.
+
+
+//do while loop
+
+var randomNumber = Math.floor(Math.random() *6) +1
+var guess;
+
+do {
+    guess = parseInt(prompt('Enter number between 1 and 6'));
+} while (guess !== randomNumber);
+
+alert('your guess of '+ guess + ' matches the number '+ randomNumber+ '!');
 
